8d33248e62c20d46e5e7faf496fa7e2739a1f76d
ROBH01 <SamFisher93gt@>
2022-04-23 22:18:34 +0100

    fix: removed header due to pre flight error

--- src/components/GameOfThrones/got-api.js
+++ src/components/GameOfThrones/got-api.js
@@ -3,7 +3,4 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://game-of-thrones-quotes.herokuapp.com/v1/house',
     timeout: 1000,
-    headers: {
-        Accept: 'Access-Control-Allow-Origin: *',
-    },
 });
